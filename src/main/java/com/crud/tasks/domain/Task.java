d37673eb2c0e5dc161a8e294b72a196c23a13204
Soroczka <olgasoroka94@yahoo.com>
2018-04-27 21:15:16 +0200

    15.2

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -3,14 +3,14 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     private Long id;
     private String title;
     private String content;
 
-    public Task() {
-    }
 }
---
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+
+public class TaskMapper {
+    public Task mapToTask(final TaskDto taskDto) {
+        return new Task(
+
+        );
+    }
+}
